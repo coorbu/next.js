05d2f383bc2212b4cdaedcc72b645ce685e7df13
Sebastian Sebbie Silbermann <sebastian.silbermann@vercel.com>
2025-02-07 18:32:51 +0100

    Update assertions

--- test/development/app-dir/missing-required-html-tags/index.test.ts
+++ test/development/app-dir/missing-required-html-tags/index.test.ts
@@ -63,22 +63,7 @@ describe('app-dir - missing required html tags', () => {
       )
     )
 
-    await openRedbox(browser)
     // TODO(NDX-768): Should show "missing tags" error
-    expect(await getRedboxDescription(browser)).toMatchInlineSnapshot(`
-      "In HTML, <p> cannot be a child of <#document>.
-      This will cause a hydration error."
-      `)
-    expect(await getRedboxTotalErrorCount(browser)).toBe(1)
-
-    // Fix the issue again
-    await next.patchFile('app/layout.js', (code) =>
-      code.replace(
-        'return children',
-        'return <html><body>{children}</body></html>'
-      )
-    )
-
     await assertNoRedbox(browser)
   })
 })
